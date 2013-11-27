dd218bce6bdbb25b38db6b54eefebd74f131adfe
He Lin <lin-he-he@163.com>
2013-11-27 22:33:15 +0800

    加了一次闭运算。然后消去过小的区域。

--- textReader/textReader/textReader.cpp
+++ textReader/textReader/textReader.cpp
@@ -6,7 +6,14 @@ using namespace std;
 CvSeq * cvxFindContourFromImage(IplImage * src_img,CvMemStorage * storage,double threshold=127,int method=CV_RETR_TREE){
 	IplImage * src_gray=cvxCreateImageSimilar(src_img,1);
 	cvCvtColor(src_img,src_gray,CV_BGR2GRAY);
+	//cvAdaptiveThreshold(src_gray,src_gray,255);
+	cvErode(src_gray,src_gray,0,1);
+	cvDilate(src_gray,src_gray,0,1);
 	cvThreshold(src_gray,src_gray,threshold,255,CV_THRESH_BINARY);
+	cvNamedWindow("img_temp");
+	cvShowImage("img_temp",src_gray);
+	cvWaitKey(0);
+
 	if(storage==NULL)storage=cvCreateMemStorage();
 	CvSeq *contours;
 	cvFindContours(src_gray,storage,&(contours),88,CV_RETR_TREE);
@@ -25,31 +32,21 @@ CvSeq * cvxGetSubSeq(CvSeq * src_seq)
 	
 	return newSeq;
 }
+void cvxBoundingContours(IplImage * src_img,CvSeq * src_contours,CvScalar color=cvScalar(rand()%255,rand()%255,rand()%255)){
+	if(src_contours->h_next)cvxBoundingContours(src_img,src_contours->h_next);
+	if(src_contours->v_next)cvxBoundingContours(src_img,src_contours->v_next,color);
+	CvRect rect=cvBoundingRect(src_contours);
+	if((rect.height>15 || rect.width>15)){
+		cvRectangle(src_img,cvPoint(rect.x,rect.y),cvPoint(rect.x+rect.width,rect.y+rect.height),color,1);
+	}
+}
 int main(){
-	IplImage * src_img=cvLoadImage("E://Linhehe//CVLab//img//text.png");
+	IplImage * src_img=cvLoadImage("E://Linhehe//CVLab//img//text3.png");
+	IplImage * erode_img=cvxCreateImageSimilar(src_img);
 	IplImage * contour_draw=cvxCreateImageSimilar(src_img);
 	CvMemStorage *storage=cvCreateMemStorage();
-	CvSeq *contours=cvxFindContourFromImage(src_img,storage,240);
-
-	int idx=0;	
-	char *win_name[300];
-	contours=contours->v_next;
-	while(contours){
-		CvRect rect=cvBoundingRect(contours);
-		//cvSetImageROI(src_img,rect);
-		cvRectangle(src_img,cvPoint(rect.x,rect.y),cvPoint(rect.x+rect.width,rect.y+rect.height),cvScalar(rand()%255,rand()%255,rand()%255),1);
-/*
-		IplImage * sub_img=cvCreateImage(cvSize(rect.width,rect.height),src_img->depth,src_img->nChannels);
-		cvCopy(src_img,sub_img);
-		win_name[idx]=new char[10];
-		sprintf(win_name[idx],"win%d",idx);
-
-		cvxShow(sub_img,CV_WINDOW_AUTOSIZE,win_name[idx]);
-*/
-
-		contours=contours->h_next;
-		idx++;
-	}
+	CvSeq *contours=cvxFindContourFromImage(src_img,storage,230);
+	cvxBoundingContours(src_img,contours);
 	cvxShow(src_img);
 	cvWaitKey(0);
 }
